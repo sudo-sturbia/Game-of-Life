becb93957853f6fa96057ce51d22eb2d4a385c89
sudo-sturbia <49781778+sudo-sturbia@users.noreply.github.com>
2019-10-05 23:10:13 +0200

    Added interface to be implemented by game class.

---
+++ src/main/java/life/implementableGame.java
@@ -0,0 +1,17 @@
+package life;
+
+/**
+ * Main game interface.
+ * Used to specify methods to be implemented by the game.
+ */
+public interface implementableGame {
+    /**
+     * Find next configuration of the game based on the current configuration.
+     */
+    void findNextConfiguration();
+
+    /**
+     * Count the number of neighbours for each square of the state grid.
+     */
+    void countNeighbours();
+}
