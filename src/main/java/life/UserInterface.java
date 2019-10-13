10ead37eba66da96d431b62d950ee8e71b51f770
sudo-sturbia <49781778+sudo-sturbia@users.noreply.github.com>
2019-10-13 23:09:59 +0200

    Created initial layout elements.

--- src/main/java/life/UserInterface.java
+++ src/main/java/life/UserInterface.java
@@ -3,8 +3,8 @@ package life;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.*;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -12,8 +12,6 @@ import javafx.stage.Stage;
  * Defines UI used in the application.
  */
 public class UserInterface extends Application {
-    // Application's main layout pane
-    private BorderPane mainPane;
 
     public static void main(String[] args) {
         launch(args);
@@ -21,20 +19,20 @@ public class UserInterface extends Application {
 
     /**
      * Initializes program.
+     *
      * @param primaryStage application's main stage.
      */
     @Override
     public void start(Stage primaryStage) {
-        // Create panes used in the application
-        mainPane = new BorderPane();
 
-        createTitle();
+        // Create and initialize program's main layout
+        final GridPane mainPane = new GridPane();
+        initMainLayout(mainPane);
 
-        // Find initial grid
-        // ...
+        createInitialLayoutElements(mainPane);
 
         // Create main scene
-        Scene scene = new Scene(mainPane, 500, 500);
+        Scene scene = new Scene(mainPane, 750, 750);
 
         primaryStage.setTitle("Game of Life");
         primaryStage.setScene(scene);
@@ -43,18 +41,95 @@ public class UserInterface extends Application {
     }
 
     /**
-     * Create game title.
+     * Initialize main layout pane and set defaults.
      */
-    private void createTitle() {
-        // Create title text and insert in top of border pane
-        StackPane topPane = new StackPane();
+    private void initMainLayout(GridPane mainPane) {
+
+        mainPane.setId("main-pane");
+
+        // Set padding and gaps
+        mainPane.setPadding(new Insets(20));
+        mainPane.setVgap(10);
+        mainPane.setHgap(10);
+
+        // Set column constraints
+        // Column 1: %75, Column 2: %25
+        ColumnConstraints colConstraint1 = new ColumnConstraints();
+        ColumnConstraints colConstraint2 = new ColumnConstraints();
+
+        colConstraint1.setPercentWidth(75);
+        colConstraint2.setPercentWidth(25);
+
+        mainPane.getColumnConstraints().addAll(colConstraint1, colConstraint2);
+
+        // Set row constraints
+        // Row 1: %25, Row 2: %75
+        RowConstraints rowConstraint1 = new RowConstraints();
+        RowConstraints rowConstraint2 = new RowConstraints();
+
+        rowConstraint1.setPercentHeight(25);
+        rowConstraint2.setPercentHeight(75);
+
+        mainPane.getRowConstraints().addAll(rowConstraint1, rowConstraint2);
+    }
+
+    /**
+     * Create initial layout elements to be displayed before game start.
+     */
+    private void createInitialLayoutElements(GridPane mainPane) {
+
+        /*
+        Create game title inside a StackPane
+         */
+        StackPane titlePane = new StackPane();
         Text title = new Text("Game of Life");
         title.setId("title-text");
 
-        topPane.getChildren().add(title);
-        topPane.setPadding(new Insets(40));
+        titlePane.getChildren().add(title);
+        titlePane.setPadding(new Insets(40));
 
-        // Add stack pane to top part of main pane
-        mainPane.setTop(topPane);
+        // Add stack pane to top corner of main pane
+        mainPane.add(titlePane, 0, 0);
+
+        /*
+        Create initial game grid
+         */
+        GridPane initialGrid = new GridPane();
+
+        // Set grid gaps
+        initialGrid.setVgap(1);
+        initialGrid.setHgap(1);
+
+        // Create a default 20 x 20 grid of equal columns and rows
+        // Set constraints
+        ColumnConstraints[] colConstraints = new ColumnConstraints[20];
+        RowConstraints[] rowConstraints = new RowConstraints[20];
+
+        for (int i = 0; i < 20; i++)
+        {
+            colConstraints[i] = new ColumnConstraints();
+            colConstraints[i].setPercentWidth(5);          // Each column represents 2% of the grid
+        }
+
+        for (int i = 0; i < 20; i++)
+        {
+            rowConstraints[i] = new RowConstraints();
+            rowConstraints[i].setPercentHeight(5);          // Each row represents 2% of the grid
+        }
+
+        initialGrid.getColumnConstraints().addAll(colConstraints);
+        initialGrid.getRowConstraints().addAll(rowConstraints);
+
+        mainPane.add(initialGrid, 0, 1);
+
+        /*
+        Create initial iterations label inside of a StackPane
+         */
+        StackPane iterationsPane = new StackPane();
+        Label iterations = new Label("Iteration: 0");
+
+        iterationsPane.getChildren().add(iterations);
+
+        mainPane.add(iterationsPane, 1, 1);
     }
 }
