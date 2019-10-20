3b5cd3e0d8d5fc4e2a7588d9c36ec7cbc82e02c7
sudo-sturbia <49781778+sudo-sturbia@users.noreply.github.com>
2019-10-20 15:16:38 +0200

    Added a seperate Main class to facilitate creating a JAR.

---
+++ src/main/java/life/Main.java
@@ -0,0 +1,11 @@
+package life;
+
+/**
+ * Initializes the JavaFX class.
+ * Implemented as a work around to create a program JAR.
+ */
+public class Main {
+    public static void main(String[] args) {
+        UserInterface.main(args);
+    }
+}
